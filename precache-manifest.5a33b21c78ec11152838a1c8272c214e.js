38d051d4fe660a2f1de9289254b7e4e675ff82b9
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-05-18 18:43:10 +0530

    Updates

---
+++ precache-manifest.5a33b21c78ec11152838a1c8272c214e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1202560252ae9863bc39981a6ecab72c",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "388c4ef703c7b33bc0f3",
+    "url": "/TodoList/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85cbd2eb19d5f27ce34e",
+    "url": "/TodoList/static/js/2.b1903564.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.b1903564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388c4ef703c7b33bc0f3",
+    "url": "/TodoList/static/js/main.5b817a06.chunk.js"
+  },
+  {
+    "revision": "0361186e564f6cc01b58",
+    "url": "/TodoList/static/js/runtime-main.9cc9eda5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
